bc4a2eb282f75bf6da6269d5d6946c753b0fab6c
Paul LeMarquand <plemarquand@qnx.com>
2017-02-07 18:16:18 -0500

    Expand eslint's coverage

--- cfg/test.js
+++ cfg/test.js
@@ -40,6 +40,6 @@ module.exports = {
     'react/lib/ExecutionEnvironment': true,
     'react/lib/ReactContext': 'window',
     'react/addons': true,
-    colors: 'window',
+    colors: 'window'
   }
 };
